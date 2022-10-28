adefd83381f76a56d028ccccab88e819d3df45bd
Emerson <91573938+Emerson007700@users.noreply.github.com>
2022-10-28 18:57:07 -0300

    teste novo

---
+++ testes/bibliotecas.py
@@ -0,0 +1,15 @@
+import math
+#from math import sqrt
+import random # numeros aleatore
+import emoji
+print(emoji.emojize('Ola, Mundo :sunglasses:',use_aliases=True)
+
+n1 = float(input('Digite um numro: '))
+raiz = math.sqrt(n1)
+print('A raiz quadrada de {} é {:.2f}'.format(n1, raiz))
+
+n2 = random.random() # dessa forma ele vai fornecer um numero aleatoreo
+n3 = random.randint(1,100) # Aqui ele vai sortear um numero (inteiro) de 1 a 10 ou seja o paramentro que foi informado
+print(n2)
+print(n3)
+)
