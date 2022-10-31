b44bc8a55d1e7c1487961da95f9152db22c693b7
Emerson <91573938+Emerson007700@users.noreply.github.com>
2022-10-31 16:46:33 -0300

    testes

--- testes/condicionais.py
+++ testes/condicionais.py
@@ -1,15 +1,31 @@
-nome=str(input('Qual seu nome? '))
-if nome == 'Emerson':
-    print('Você será um exelente programador python')
+# (if - significa (se) em bloco e vai ser o bloco verdadeiro (True))
+# (else - significa (se não) e vai ser o bloco falso (False)
+# Esses são os primeiros comandos condicionais.
+# Exempos
+tempo = int(input('Quanto tempo tem seu carro: '))
+if tempo <= 3:
+    print('Por seu carro ter {} anos ele atende os requsitos necessarios'.format(tempo))
 else:
-    print('Sinto muito mas você não receberá o premio')
-print('Bom dia {}!'.format(nome))
+    print('Por seu carro ter mais de {} anos ele nao atende o requisito.'.format(tempo))
+print('Fim')
 
-n1=float(input('Qual a nota da sua prova? '))
-n2=float(input('Qual a nota da sua segunda prova?'))
-m=(n1+n2)/2
-print('Sua média é {}.'.format(m))
-if m>=6.0:
-    print('(Aprovado) \nVoçê esta na média, parabens.')
+# Ou simplificando veja o exemplo
+carro = int(input('Quantos anos tem seu carro: '))
+print('carro novo'if carro <= 3 else 'Carro velho')
+
+nome = str(input('Qual seu nome: '))
+if nome == "Emerson":
+    print('Paraben voce foi selecionado.')
 else:
-    print('(Reprovado) \nVoçê esta abaixo da média permitida, por vafor estude mais.')
+    print('Não foi dessa vez.')
+print('Bom dia')
+
+n1 = float(input('Digite sua primeira nota: '))
+n2 = float(input('Digite sua segunda nota: '))
+m = (n1+n2)/2
+print('Sua média foi {}'.format(m))
+if m >= 6.0:
+    print("Parabens você foi aprovado.")
+else:
+    print('Voce reprovou.')
+
